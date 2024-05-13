b2a6aac781b2d9abc8cdf8eb85c5c00975d37796
Emmerjo <jonathan.emmers@pxl.be>
2024-05-13 22:24:05 +0200

    Add env file

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
 
 func main() {
 	fmt.Println("Hello World")
+
+	godotenv.Load()
+
+	portSTring := os.Getenv("PORT")
+	if portSTring == "" {
+		log.Fatal("Geen port ingesteld in de env")
+	}
+
+	fmt.Println("Port:", portSTring)
 }
