c8083b31459f00a4530055aa343425e536aec7be
Emmerjo <jonathan.emmers@pxl.be>
2024-05-13 23:45:22 +0200

    Set up basis

--- go.mod
+++ go.mod
@@ -3,3 +3,7 @@ module github.com/emmerjo/goprojectrss
 go 1.22.3
 
 require github.com/joho/godotenv v1.5.1
+
+require github.com/go-chi/chi v1.5.5
+
+require github.com/go-chi/cors v1.2.1
--- main.go
+++ main.go
@@ -1,16 +1,16 @@
 package main
 
 import (
-	"fmt"
 	"log"
+	"net/http"
 	"os"
 
+	"github.com/go-chi/chi"
+	"github.com/go-chi/cors"
 	"github.com/joho/godotenv"
 )
 
 func main() {
-	fmt.Println("Hello World")
-
 	godotenv.Load()
 
 	portSTring := os.Getenv("PORT")
@@ -18,5 +18,31 @@ func main() {
 		log.Fatal("Geen port ingesteld in de env")
 	}
 
-	fmt.Println("Port:", portSTring)
+	router := chi.NewRouter()
+
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"*"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300,
+	}))
+
+	v1Router := chi.NewRouter()
+	v1Router.Get("/healthz", handlerReadiness)
+	v1Router.Get("/err", handlerErr)
+
+	router.Mount("/v1", v1Router)
+
+	srv := &http.Server{
+		Handler: router,
+		Addr:    ":" + portSTring,
+	}
+
+	log.Printf("Server gestart op poort %v", portSTring)
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
