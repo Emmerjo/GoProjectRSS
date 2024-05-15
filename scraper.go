658ab1be1f45133a2c501fdffe45f8af378f5d54
Emmerjo <jonathan.emmers@pxl.be>
2024-05-15 19:53:56 +0200

    Finish post functionality

--- scraper.go
+++ scraper.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"context"
+	"database/sql"
 	"log"
+	"strings"
 	"sync"
 	"time"
 
 	"github.com/emmerjo/goprojectrss/internal/database"
+	"github.com/google/uuid"
 )
 
 func startScraping(
@@ -52,7 +55,35 @@ func scrapeFeed(db *database.Queries, wg *sync.WaitGroup, feed database.Feed) {
 	}
 
 	for _, item := range rssFeed.Channel.Item {
-		log.Println("Post gevonden", item.Title, "op feed", feed.Name)
+		description := sql.NullString{}
+		if item.Description != "" {
+			description.String = item.Description
+			description.Valid = true
+		}
+
+		pubAt, err := time.Parse(time.RFC1123Z, item.PubDate)
+		if err != nil {
+			log.Printf("Kon publicatiedatum niet parsen %v met error %v", item.PubDate, err)
+			continue
+		}
+		_, err = db.CreatePost(context.Background(),
+			database.CreatePostParams{
+				ID:          uuid.New(),
+				CreatedAt:   time.Now().UTC(),
+				UpdatedAt:   time.Now().UTC(),
+				Title:       item.Title,
+				Description: description,
+				PublishedAt: pubAt,
+				Url:         item.Link,
+				FeedID:      feed.ID,
+			})
+		if err != nil {
+			if strings.Contains(err.Error(), "duplicate key") {
+				continue
+			}
+			log.Println("Kon post niet aanmaken met error:", err)
+		}
 	}
+
 	log.Printf("Feed %s verzameld, %v posts gevonden", feed.Name, len(rssFeed.Channel.Item))
 }
